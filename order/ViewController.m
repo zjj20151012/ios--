e15ac38e6ffef608a79a06203cd5bcc9eac87fc9
zjjbitbucket <443411162@qq.com>
2016-03-07 21:51:08 +0800

    Initial Commit

---
+++ order/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  order
+//
+//  Created by zjj on 16/3/7.
+//  Copyright © 2016年 zjj. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ order/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  order
+//
+//  Created by zjj on 16/3/7.
+//  Copyright © 2016年 zjj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
